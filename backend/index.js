9930914d0fc2c440213b8363837156f5dc132190
Vinay <vinay00mahto@gmail.com>
2025-06-07 23:56:44 +0530

    adding mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+import adminRouter from './routes/adminroute.js';
 
 // app config
 const app = express()
 const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
 
 // middlewares
 app.use(express.json())
@@ -12,6 +17,9 @@ app.use(cors())
 
 
 // api endpoint
+app.use('/api/admin',adminRouter)
+// localhost:4000/api/admin/add-doctor
+
 app.get('/',(req,res)=>{
     res.send('API WORKING')
 })
