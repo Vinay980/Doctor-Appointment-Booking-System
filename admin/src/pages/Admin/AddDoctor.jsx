7e3f64f1eca047c0f4798145cc561cf9905c4fa6
Vinay <vinay00mahto@gmail.com>
2025-06-14 23:42:44 +0530

    add

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../../assets/assets'
 
 function AddDoctor() {
+  const [docImg,setDocImg] = useState(false)
+  const [name,setName] = useState('')
+  const [email,setEmail] = useState('')
+  const [password,setPassword] = useState('')
+  const [experience,setExperience] = useState('1 Year')
+  const [fee,setFee] = useState('')
+  const [about,setAbout] = useState('')
+  const [speciality,setSpeciality] = useState('General Physician')
+  const [degree,setDegree] = useState('')
+  const [address1,setAddress1] = useState('')
+  const [address2,setAddress2] = useState('')
+
+  const onSubmitHandler = async (event) =>{
+    event.preventDefault()
+  }
+
   return (
-    <form action="" className='m-5 w-full'>
+    <form onSubmit={onSubmitHandler} action="" className='m-5 w-full'>
       <p className='mb-3 text-lg font-medium'>Add Doctor</p>
       <div className='bg-white px-8 py-8 border rounded w-full max-w-4xl max-h-[80vh] overflow-y-scroll'>
         <div className='flex items-center gap-4 mb-8 text-gray-500'>
           <label htmlFor="doc-img">
-            <img className='w-16 bg-gray-100 rounded-full cursor-pointer' src={assets.upload_area} alt="" />
+            <img className='w-16 bg-gray-100 rounded-full cursor-pointer' src={docImg ? URL.createObjectURL(docImg) : assets.upload_area} alt="" />
           </label>
-          <input type="file" name="" id="doc-img" hidden />
+          <input onChange={(e)=>setDocImg(e.target.files[0])} type="file" name="" id="doc-img" hidden />
           <p>Upload doctor <br /> picture</p>
         </div>
 
@@ -18,22 +34,22 @@ function AddDoctor() {
           <div className='w-full lg:flex-1 flex flex-col gap-4'>
             <div className="flex-1 flex flex-col gap-1">
               <p>Doctor Name</p>
-              <input className='border rounded px-3 py-2' type="text" placeholder='Name' required />
+              <input onChange={(e)=>setName(e.target.value)} value={name} className='border rounded px-3 py-2' type="text" placeholder='Name' required />
             </div>
 
             <div className="flex-1 flex flex-col gap-1">
               <p>Doctor Email</p>
-              <input className='border rounded px-3 py-2' type="email" placeholder='Email' required />
+              <input onChange={(e)=>setEmail(e.target.value)} value={email} className='border rounded px-3 py-2' type="email" placeholder='Email' required />
             </div>
 
             <div className="flex-1 flex flex-col gap-1">
               <p>Doctor Password</p>
-              <input className='border rounded px-3 py-2' type="password" placeholder='Password' required />
+              <input onChange={(e)=>setPassword(e.target.value)} value={password} className='border rounded px-3 py-2' type="password" placeholder='Password' required />
             </div>
 
             <div className="flex-1 flex flex-col gap-1">
               <p>Experience</p>
-              <select className='border rounded px-2 py-2' name="" id="">
+              <select onChange={(e)=>setExperience(e.target.value)} value={experience} className='border rounded px-2 py-2' name="" id="">
                 <option value="1 Year">1 Year</option>
                 <option value="2 Year">2 Year</option>
                 <option value="3 Year">3 Year</option>
@@ -48,14 +64,14 @@ function AddDoctor() {
             </div>
             <div className="flex-1 flex flex-col gap-1">
               <p>Fees</p>
-              <input className="border rounded px-3 py-2" type="number" placeholder="Doctor fees" required="" value="" />
+              <input onChange={(e)=>setFee(e.target.value)} className="border rounded px-3 py-2" type="number" placeholder="Doctor fees" required="" value={fee} />
             </div>
           </div>
 
           <div>
             <div className="flex-1 flex flex-col gap-1">
               <p>Speciality</p>
-              <select className="border rounded px-2 py-2">
+              <select onChange={(e)=>setSpeciality(e.target.value)} value={speciality} className="border rounded px-2 py-2">
                 <option value="General physician">General physician</option>
                 <option value="Gynecologist">Gynecologist</option>
                 <option value="Dermatologist">Dermatologist</option>
@@ -66,18 +82,18 @@ function AddDoctor() {
             </div>
             <div className="flex-1 flex flex-col gap-1">
               <p>Degree</p>
-              <input className="border rounded px-3 py-2" type="text" placeholder="Degree" required="" value="" />
+              <input onChange={(e)=>setDegree(e.target.value)} className="border rounded px-3 py-2" type="text" placeholder="Degree" required="" value={degree} />
             </div>
             <div className="flex-1 flex flex-col gap-1">
               <p>Address</p>
-              <input className="border rounded px-3 py-2" type="text" placeholder="Address 1" required="" value="" />
-              <input className="border rounded px-3 py-2" type="text" placeholder="Address 2" required="" value="" />
+              <input onChange={(e)=>setAddress1(e.target.value)} className="border rounded px-3 py-2" type="text" placeholder="Address 1" required="" value={address1} />
+              <input onChange={(e)=>setAddress2(e.target.value)} className="border rounded px-3 py-2" type="text" placeholder="Address 2" required="" value={address2} />
             </div>
           </div>
         </div>
         <div>
           <p className="mt-4 mb-2">About Doctor</p>
-          <textarea className="w-full px-4 pt-2 border rounded" rows={5} placeholder="write about doctor">
+          <textarea onChange={(e)=>setAbout(e.target.value)} value={about} className="w-full px-4 pt-2 border rounded" rows={5} placeholder="write about doctor">
           </textarea>
         </div>
         <button type="submit" className="bg-[#5f6fff] px-10 py-3 mt-4 text-white rounded-full">Add doctor</button>
